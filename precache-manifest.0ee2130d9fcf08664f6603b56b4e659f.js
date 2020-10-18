fd96d5c168a4cde5569d1dd5311cc2632363e8f8
Alons21 <alonso@alonsoROG.localdomain>
2020-10-18 23:00:18 +0200

    Updates

---
+++ precache-manifest.0ee2130d9fcf08664f6603b56b4e659f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aace3b3f40f148bee75df58f91dc49bb",
+    "url": "/js-calculator/index.html"
+  },
+  {
+    "revision": "1640d8ab8a3b20efc83b",
+    "url": "/js-calculator/static/css/main.3353dec5.chunk.css"
+  },
+  {
+    "revision": "1067aba41061454e3ae4",
+    "url": "/js-calculator/static/js/2.6b3686b1.chunk.js"
+  },
+  {
+    "revision": "846e3710ca5ee3f911296f05447fe08c",
+    "url": "/js-calculator/static/js/2.6b3686b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1640d8ab8a3b20efc83b",
+    "url": "/js-calculator/static/js/main.d48d1f89.chunk.js"
+  },
+  {
+    "revision": "7678ef8a913a10498f86",
+    "url": "/js-calculator/static/js/runtime-main.bdbd5d51.js"
+  },
+  {
+    "revision": "ce4b0ee545ec52c2ec8cec62d23ffb09",
+    "url": "/js-calculator/static/media/Calculator.ce4b0ee5.ttf"
+  }
+]);
